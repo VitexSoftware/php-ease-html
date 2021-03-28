d980f432b8283420f8ed91da938cf83ce826b2a8
Vítězslav Dvořák <info@vitexsoftware.cz>
2021-03-28 20:25:11 +0200

    Formated && extended

--- Examples/table.php
+++ Examples/table.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * EaseFramework - HTML5 Table Example
  *
@@ -8,13 +9,12 @@
 
 namespace Example\Ease\HTML;
 
-require_once __DIR__.'/../vendor/autoload.php';
-
+require_once __DIR__ . '/../vendor/autoload.php';
 
 $tableData = [
-    'head' => ['col1'=>'col1 heading','col2'=>'col2 heading', 'col3'=>'col3 heading'],
-    'body' => ['col1'=>'col1 body','col2'=>'col2 body', 'col3'=>'col3 body'],
-    'foot' => ['col1'=>'col1 footer','col2'=>'col2 footer', 'col3'=>'col3 footer'],
+    'head' => ['col1' => 'col1 heading', 'col2' => 'col2 heading', 'col3' => 'col3 heading'],
+    'body' => ['col1' => 'col1 body', 'col2' => 'col2 body', 'col3' => 'col3 body'],
+    'foot' => ['col1' => 'col1 footer', 'col2' => 'col2 footer', 'col3' => 'col3 footer'],
 ];
 
 $table = new \Ease\Html\TableTag();
@@ -22,5 +22,35 @@ $table = new \Ease\Html\TableTag();
 $table->addRowHeaderColumns($tableData['head']);
 $table->addRowColumns($tableData['body']);
 $table->addRowFooterColumns($tableData['foot']);
-        
+
 echo $table;
+
+
+/**
+Produced code:
+
+<table>
+   <thead>
+      <tr>
+         <th>col1 heading</th>
+         <th>col2 heading</th>
+         <th>col3 heading</th>
+      </tr>
+   </thead>
+   <tbody>
+      <tr>
+         <td>col1 body</td>
+         <td>col2 body</td>
+         <td>col3 body</td>
+      </tr>
+   </tbody>
+   <tfoot>
+      <tr>
+         <th>col1 footer</th>
+         <th>col2 footer</th>
+         <th>col3 footer</th>
+      </tr>
+   </tfoot>
+</table>
+
+*/
