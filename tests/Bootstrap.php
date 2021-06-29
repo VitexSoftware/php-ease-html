5079f9572d42ddcea3ed45849756c942a1763b04
Vítězslav Dvořák <info@vitexsoftware.cz>
2021-06-29 20:26:12 +0200

    special user for unit test

--- tests/Bootstrap.php
+++ tests/Bootstrap.php
@@ -7,8 +7,6 @@ declare (strict_types=1);
  * @copyright  2012-2017 Vitex@hippy.cz (G)
  */
 
-namespace Test\Ease;
-
 require_once __DIR__.'/../vendor/autoload.php';
 
 if ((php_sapi_name() != 'cli') && (session_status() == 'PHP_SESSION_NONE')) {
@@ -22,9 +20,10 @@ define('EASE_LOGGER', 'syslog');
 
 
 \Ease\Locale::singleton('cs_CZ');
-\Ease\Shared::user(new \Ease\Anonym());
 //\Ease\Shared::webPage(new \Ease\WebPage());
 
 class User extends \Ease\User {
-  
+ 
 }
+
+\Ease\Shared::user(new \User);
