77838e7bdd2a26dfa29e2f959f0511abb5505351
CyberVitexus <info@vitexsoftware.cz>
2023-07-25 14:34:52 +0200

    Update for php8.2 II

--- tests/src/Ease/Html/BodyTagTest.php
+++ tests/src/Ease/Html/BodyTagTest.php
@@ -15,14 +15,7 @@ class BodyTagTest extends PairTagTest {
      * @var BodyTag
      */
     protected $object;
-    public $rendered = '<body>
-
-<script>
-// <![CDATA[
-window.location = "login.php"
-// ]]>
-</script>
-</body>';
+    public $rendered = '<body></body>';
 
     /**
      * Sets up the fixture, for example, opens a network connection.
